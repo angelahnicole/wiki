37d7e01cb8d0c0c956fcaa10b323724d2d19c47c
angelahnicole <angelangross@gmail.com>
2016-04-04 19:11:22 -0600

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,3 +1,13 @@
 <?php
-
-// Removing routes- manually creating them in order to use subdomain routing
+Route::group(['domain' => 'wiki.stellar.{tld}'], function () 
+{
+  Route::group(['namespace' => 'Taskforcedev\Wiki\Http\Controllers'], function() 
+  {
+  
+    Route::get('wiki/install', ['as' => 'wiki.install', 'uses' => 'AdminController@install']);
+    
+    Route::get('wiki/{page}', ['as' => 'wiki.page.view', 'uses' => 'WikiController@view']);
+    Route::post('wiki/page', ['as' => 'wiki.page.create', 'uses' => 'WikiController@create']);
+  
+  });
+});
