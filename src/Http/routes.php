a210de427d114a212f1cff7ddf0319af13732aac
angelahnicole <angelangross@gmail.com>
2016-04-04 19:13:38 -0600

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,10 +4,10 @@ Route::group(['domain' => 'wiki.stellar.polymorphixgaming.com'], function ()
   Route::group(['namespace' => 'Taskforcedev\Wiki\Http\Controllers'], function() 
   {
   
-    Route::get('wiki/install', ['as' => 'wiki.install', 'uses' => 'AdminController@install']);
+    Route::get('install', ['as' => 'wiki.install', 'uses' => 'AdminController@install']);
     
-    Route::get('wiki/{page}', ['as' => 'wiki.page.view', 'uses' => 'WikiController@view']);
-    Route::post('wiki/page', ['as' => 'wiki.page.create', 'uses' => 'WikiController@create']);
+    Route::get('{page}', ['as' => 'wiki.page.view', 'uses' => 'WikiController@view']);
+    Route::post('page', ['as' => 'wiki.page.create', 'uses' => 'WikiController@create']);
   
   });
 });
